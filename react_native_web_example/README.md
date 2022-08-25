4f97d637b3e3edc9da897643b38e001533574494
NicoleSelig <nlselig@sep.com>
2022-08-25 17:16:09 -0400

    delete other example

--- react_native_web_example/README.md
+++
@@ -1,6 +0,0 @@
-# addon_react_native_web_example
-An example of the storybook addon in action
-
-get setup with `yarn install`
-
-Run storybook with `yarn storybook`
--- react_native_web_example/components/libraries/VectorIcons/Icon.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import {materialIcons} from './icons.type';
-
-type Props = {name: keyof materialIcons};
-export const Icon = ({name}: Props) => {
-  return <MaterialCommunityIcons size={72} name={name} />;
-};
